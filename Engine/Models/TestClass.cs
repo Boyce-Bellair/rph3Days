ba1550f1396d1f670ddf90552b2458ee755ae9fa
Benjamin Boyce <boyceb@gmail.com>
2020-08-25 05:24:19 -0700

    Add project files.

---
+++ Engine/Models/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class TestClass : BaseNotificationClass
+    {
+        private string test = "test";
+
+        public string Test
+        {
+            get { return test; }
+            set
+            {
+                test = value;
+                OnPropertyChanged(nameof(Test));
+            }
+        }
+    }
+}
---
+++ Engine/Models/Trader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Trader : BaseNotificationClass
+    {
+        public string Name { get; set; }
+        public ObservableCollection<GameItem> inventory { get; set; }
+
+        public Trader(string name)
+        {
+            Name = name;
+            inventory = new ObservableCollection<GameItem>();
+        }
+        public void AddItemToInventory(GameItem item)
+        {
+            inventory.Add(item);
+        }
+
+        public void RemoveItemFromInventory(GameItem item)
+        {
+            inventory.Remove(item);
+        }
+
+    }
+}
